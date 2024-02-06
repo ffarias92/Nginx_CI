2f87b166d39267d4e7b182f62bdd103b26ceb271
Pipe <ffariasg92@gmail.com>
2024-02-06 19:42:47 -0300

    corregido archivo variables

--- database.sh
+++ database.sh
@@ -1,9 +1,21 @@
+#!/bin/bash
+
+# Crear el archivo variables_database.tf
+touch variables_database.tf
+
+# Escribir las definiciones de variables en el archivo
+echo '
 variable "database_username" {
-type    = string
-default = "usuario1"
+  type    = string
+  default = "usuario1"
 }
 
 variable "database_password" {
-type    = string
-default = "contraseña1"
+  type    = string
+  default = "contraseña1"
 }
+' > variables_database.tf
+
+# Dar formato al archivo con terraform fmt
+terraform fmt variables_database.tf
+
---
+++ variables_database.tf
@@ -0,0 +1,11 @@
+
+variable "database_username" {
+  type    = string
+  default = "usuario1"
+}
+
+variable "database_password" {
+  type    = string
+  default = "contraseña1"
+}
+
