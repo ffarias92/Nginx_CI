21e5171fd49cade933abcc293e06a72cb13c0d8d
Pipe <ffariasg92@gmail.com>
2024-02-06 19:49:37 -0300

    corregido tfstate

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-# Archivo backend.tf
+## Archivo backend.tf
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-ffarias-1"
-    key    = "network/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-ffarias-1"
+#    key    = "network/terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
